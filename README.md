83bd5ed72d352ca629e49c1cfe25876ec22dfa55
赵欣 <zhaoxin@qiyestore.com>
2016-01-27 18:03:53 +0800

    更新v2

---
+++ README.md
@@ -0,0 +1,54 @@
+
+# 安装
+下载源码
+```sh
+ionic plugin add http://git.qiyestore.com/zhuanmi/cordova-plugin-sharesdk.git \
+	--variable SHARESDKAPPKEY=f2629488c78c \
+	--variable QQAPPID=1105143220 \
+	--variable QQAPPKEY=vBktd0sFOFLLkpLP \
+	--variable WECHATAPPID=wx739a973d597b2a12 \
+	--variable WECHATAPPSECRET=4eec3804ec8f4fbbed2959e8c210fc42 \
+	--variable QQURLSCHEME=QQ41DF25B4 \
+	--variable WBAPPKEY=4289465453 \
+	--variable WBAPPSECRET=de2069dd056a313b76306ee376eecc0f \
+	--variable WBREDIRECTURL=http://www.zhuanmi.cc
+```
+
+|参数|说明|
+|---|---|
+|SHARESDKAPPKEY|ShareSDK注册|
+|QQAPPID|QQ开放平台注册|
+|QQAPPKEY|QQ开放平台注册|
+|QQURLSCHEME|QQ回调Scheme。例如:`QQ41DF25B4`|
+|WECHATAPPID|微信开放平台注册|
+|WECHATAPPSECRET|微信开放平台注册|
+|WBAPPKEY|新浪微博|
+|WBAPPSECRET|新浪微博|
+|WBREDIRECTURL|新浪微博回调地址。必须与注册时一致|
+
+## 卸载重新安装
+
+```sh
+ionic plugin remove com.ryanzx.cordova.plugin.sharesdk
+```
+
+
+# JS调用
+
+```js
+function test() {
+	cordova.exec(function(result) {
+		alert('分享成功');
+    }, function(error) {
+		alert('分享失败');
+		console.debug(error)
+    }, "ShareSDK", "send", ['测试分享标题','你们好啊这里是测试分享','http://cdn.qiyestore.com/openapi/upload/2015/12/25/EYZZ17L785.png','http://www.qiyestore.com']);
+}
+```
+
+|参数|说明|
+|---|---|
+|参数1|标题|
+|参数2|文字内容|
+|参数3|图片URL|
+|参数4|分享查看URL|
--- src/ios/ShareSDKPlugin.m
+++ src/ios/ShareSDKPlugin.m
@@ -11,6 +11,9 @@
 //微信SDK头文件
 #import "WXApi.h"
 
+//新浪微博
+#import "WeiboSDK.h"
+
 
 @implementation ShareSDKPlugin
 #pragma mark "API"
@@ -21,8 +24,12 @@
     NSString* wechatAppSecret = [[self.commandDelegate settings] objectForKey:@"wechatappsecret"];
     NSString* qqAppId = [[self.commandDelegate settings] objectForKey:@"qqappid"];
     NSString* qqAppKey = [[self.commandDelegate settings] objectForKey:@"qqappkey"];
-    if(wechatAppId && wechatAppSecret && qqAppId && qqAppKey && sharesdkAppKey){
- 		/*Share SDK config*/
+    NSString* wbAppKey = [[self.commandDelegate settings] objectForKey:@"wbappkey"];
+    NSString* wbAppSecret = [[self.commandDelegate settings] objectForKey:@"wbappsecret"];
+    NSString* wbRedirectUrl = [[self.commandDelegate settings] objectForKey:@"wbredirecturl"];
+
+    if(wechatAppId && wechatAppSecret && qqAppId && qqAppKey && wbAppKey && wbAppSecret && wbRedirectUrl && sharesdkAppKey){
+        /*Share SDK config*/
         [ShareSDK registerApp:sharesdkAppKey];
         
         [ShareSDK connectWeChatWithAppId:wechatAppId
@@ -31,37 +38,41 @@
 
         
         //添加QQ空间应用  注册网址  http://connect.qq.com/intro/login/
-	    [ShareSDK connectQZoneWithAppKey:qqAppId
-	                           appSecret:qqAppKey
-	                   qqApiInterfaceCls:[QQApiInterface class]
-	                     tencentOAuthCls:[TencentOAuth class]];
-	 
-	    //添加QQ应用  注册网址   http://mobile.qq.com/api/
-	    [ShareSDK connectQQWithQZoneAppKey:qqAppId
-	                     qqApiInterfaceCls:[QQApiInterface class]
-	                       tencentOAuthCls:[TencentOAuth class]];
+        [ShareSDK connectQZoneWithAppKey:qqAppId
+                               appSecret:qqAppKey
+                       qqApiInterfaceCls:[QQApiInterface class]
+                         tencentOAuthCls:[TencentOAuth class]];
+     
+        //添加QQ应用  注册网址   http://mobile.qq.com/api/
+        [ShareSDK connectQQWithQZoneAppKey:qqAppId
+                         qqApiInterfaceCls:[QQApiInterface class]
+                           tencentOAuthCls:[TencentOAuth class]];
 
-	     //当使用新浪微博客户端分享的时候需要按照下面的方法来初始化新浪的平台
-   		// [ShareSDK connectSinaWeiboWithAppKey:@"568898243"     
-     //    	                       appSecret:@"38a4f8204cc784f81f9f0daaf31e02e3" 
-     //           	            	 redirectUri:@"http://www.sharesdk.cn" 
-     //                	         weiboSDKCls:[WeiboSDK class]];
+        //当使用新浪微博客户端分享的时候需要按照下面的方法来初始化新浪的平台
+        [ShareSDK connectSinaWeiboWithAppKey:wbAppKey
+                                   appSecret:wbAppSecret
+                                 redirectUri:wbRedirectUrl
+                                 weiboSDKCls:[WeiboSDK class]];
 
- 	}
+    }
 }
 
 - (void)send:(CDVInvokedUrlCommand*)command {
     
+    NSString* title = [command.arguments objectAtIndex:0];
+    NSString* text = [command.arguments objectAtIndex:1];
+    NSString* imageUrl = [command.arguments objectAtIndex:2];
+    NSString* url = [command.arguments objectAtIndex:3];
+
     // CDVPluginResult* __block pluginResult = nil;
      //构造分享内容
-    id<ISSContent> publishContent = [ShareSDK content:@"分享内容"
-                                       defaultContent:@"测试一下"
-                                                image:[ShareSDK imageWithUrl:@"http://cdn.qiyestore.com/openapi/upload/2015/12/25/EYZZ17L785.png"]
-                                                title:@"测试分享哈哈哈"
-                                                  url:@"http://www.qiyestore.com"
-                                          description:@"这是一条测试信息"
+    id<ISSContent> publishContent = [ShareSDK content: text
+                                       defaultContent: text
+                                                image:[ShareSDK imageWithUrl:imageUrl]
+                                                title: title
+                                                  url: url
+                                          description: text
                                             mediaType:SSPublishContentMediaTypeNews];
-    //创建弹出菜单容器
 
     //弹出分享菜单
     [ShareSDK showShareActionSheet:nil
@@ -71,37 +82,31 @@
                        authOptions:nil
                       shareOptions:nil
                             result:^(ShareType type, SSResponseState state, id<ISSPlatformShareInfo> statusInfo, id<ICMErrorInfo> error, BOOL end) {
- 
+                                
+                                CDVPluginResult* pluginResult = nil;
+                                
                                 if (state == SSResponseStateSuccess)
                                 {
-                                    NSLog(@"分享成功");
+                                    pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsString:@"success"];
+                                    
+                                    [self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
                                 }
                                 else if (state == SSResponseStateFail)
                                 {
                                     NSLog(@"分享失败,错误码:%ld,错误描述:%@", [error errorCode], [error errorDescription]);
+                                    pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR messageAsString:[NSString stringWithFormat:@"错误码:%ld,错误描述:%@", [error errorCode], [error errorDescription]]];
+                                    
+                                    [self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
+                                }
+                                else if (state == SSResponseStateCancel)
+                                {
+                                    pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR messageAsString:@"cancel"];
+                                    
+                                    [self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
                                 }
                             }];
 
 
-
-    // //创建弹出菜单容器
-    // NSArray *shareList = [ShareSDK getShareListWithType:ShareTypeSinaWeibo,ShareTypeQQ,ShareTypeQQSpace,ShareTypeWeixiFav,ShareTypeWeixiSession,ShareTypeWeixiTimeline, nil];
-    // //弹出分享菜单
-    // [ShareSDK oneKeyShareContent:publishContent
-    //                      shareList:shareList
-    //                    authOptions:nil
-    //                  statusBarTips:YES
-    //                         result:^(ShareType type, SSResponseState state, id<ISSPlatformShareInfo> statusInfo, id<ICMErrorInfo> error, BOOL end) {
- 
-    //                             if (state == SSResponseStateSuccess)
-    //                             {
-    //                                 NSLog(@"分享成功");
-    //                             }
-    //                             else if (state == SSResponseStateFail)
-    //                             {
-    //                                 NSLog(@"分享失败,错误码:%ld,错误描述:%@", [error errorCode], [error errorDescription]);
-    //                             }
-    //                         }];
 }
 @end
 
